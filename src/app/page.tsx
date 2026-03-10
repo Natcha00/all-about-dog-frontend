fe7e14c77bded915843af0087172e4d6e05ce8eb
natcha01 <natcha24oct@gmail.com>
2026-03-11 03:13:54 +0700

    feat: implement dynamic banner fetching and enhance HomeBanner component
    - Added a new API route for fetching banner images, improving the dynamic content display on the home page.
    - Refactored HomeBanner component to utilize fetched banner data instead of static images, enhancing flexibility and maintainability.
    - Implemented touch event handling for swiping between banners and added functionality for viewing full-size images.
    - Updated StepSuccess component to include a button for navigating back to the home page, improving user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -359,7 +359,7 @@ export default function DogOwnerHomePage() {
                           onClick={() =>
                             router.push(`/service/booking/${item.id}`)
                           }
-                          className="w-full rounded-2xl bg-white px-3 py-2.5 text-left shadow-sm ring-1 ring-black/5 active:scale-[0.99] transition"
+                          className="w-full rounded-2xl bg-white px-3 py-2.5 mt-1 text-left shadow-sm ring-1 ring-black/5 active:scale-[0.99] transition"
                         >
                           <div className="flex items-center justify-between gap-2">
                             <div className="min-w-0">
--- src/components/ui/walkin/StepSuccess.tsx
+++ src/components/ui/walkin/StepSuccess.tsx
@@ -1,16 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function StepSuccess(props: { refCode: string; onReset: () => void }) {
   const { refCode, onReset } = props;
-
+  const router = useRouter();
   return (
+    <>
     <section className="rounded-3xl bg-white/70 ring-1 ring-black/5 shadow-sm p-6 space-y-4 text-center">
       <div className="text-4xl">✅</div>
       <h2 className="text-2xl font-extrabold text-gray-900">ทำรายการสำเร็จ</h2>
-      <p className="text-sm text-black/50">รายการอ้างอิง</p>
-      <div className="mx-auto w-fit rounded-2xl bg-[#F7F4E8] ring-1 ring-black/5 px-4 py-2 font-extrabold">
-        {refCode}
-      </div>
 
       <button
         type="button"
@@ -20,5 +19,13 @@ export default function StepSuccess(props: { refCode: string; onReset: () => voi
         ทำรายการใหม่
       </button>
     </section>
+        <button
+        type="button"
+        onClick={() => router.push("/")}
+        className="w-full rounded-2xl bg-black/[0.07] py-4 text-lg font-extrabold text-black active:scale-[0.99] transition"
+      >
+        กลับหน้าหลัก
+      </button>
+    </>
   );
 }
